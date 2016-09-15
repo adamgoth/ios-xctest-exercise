70a5e06a61fda426d51c365ea3e2001f937b4a6e
Adam Goth <adamgoth@gmail.com>
2016-09-15 16:55:27 -0600

    Initial Commit

---
+++ xctest-exercise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  xctest-exercise
+//
+//  Created by Adam Goth on 9/15/16.
+//  Copyright © 2016 Adam Goth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
